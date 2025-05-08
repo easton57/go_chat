8e893d3a4e7896212029af9e903348941f16464c
Easton Seidel <eseidel1357@gmail.com>
2025-05-08 16:43:21 -0700

    Created files

---
+++ sql/messaging.sql
@@ -0,0 +1,9 @@
+-- Send message
+
+-- Get messages
+
+-- Delete message
+
+-- Validate room membership
+
+-- Mark messages read
---
+++ sql/room_management.sql
@@ -0,0 +1,13 @@
+-- Create Room
+
+-- Add user to room
+
+-- Get Room ID
+
+-- Delete room
+
+-- Leave Room
+
+-- Get Room Details
+
+-- List rooms
